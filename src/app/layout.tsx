995f38b966039e85df05673f4a789146f56b0ab1
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-09-13 19:35:49 +0530

    feat: add ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,10 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
-  title: "Discord Bot CLient",
+  title: "Discord Bot Client",
   description: "Still in development",
 };
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Toaster position="top-right" />
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-1RKN83QLN1" />
       <Script src="https://api.instatus.com/widget?host=status.uncoverit.org&code=4f0eef87&locale=en" />
     </html>
   );
