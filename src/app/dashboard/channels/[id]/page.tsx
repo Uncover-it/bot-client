6277e67bb6a4f983455b250e02101d09232f7d9d
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-09-14 15:35:56 +0530

    fix: force the messagebox to always be visible

--- src/app/dashboard/channels/[id]/page.tsx
+++ src/app/dashboard/channels/[id]/page.tsx
@@ -4,7 +4,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   return (
     <main className="overflow-hidden min-h-screen flex flex-col">
       <div className="flex-1"></div>
-      <footer className="w-full items-end h-16 p-2 flex">
+      <footer className="absolute bottom-0 w-full items-end p-2 flex z-50">
         <MessageBox id={(await props.params).id} />
       </footer>
     </main>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,4 @@
-import {
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import "@/app/globals.css";
@@ -21,7 +18,7 @@ export default function RootLayout({
           <div className="absolute top-0 left-0">
             <SidebarTrigger className="mt-4.5 ml-1.5 shrink-0 items-center gap-2 transition-[width,height] ease-linear fixed " />
           </div>
-            {children}
+          {children}
           <Toaster position="top-right" />
         </main>
       </div>
