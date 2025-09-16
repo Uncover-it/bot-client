d682dbda79f8916ef28fc89e9dd9b954ee83f5f4
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-09-17 00:26:30 +0530

    fix: validate token and automatically logout

--- src/app/dashboard/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/dashboard/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import MessageBox from "@/components/messagebox";
 import { MessageList } from "@/components/messageList";
+import { getBotInfo, logout } from "@/api/data/actions";
 
 type PageProps = {
   params: {
@@ -8,13 +9,16 @@ type PageProps = {
   };
 };
 
-
 export default async function Page({ params }: PageProps) {
   const { serverId, channelId } = await params;
+  const data = await getBotInfo();
+  if (!data.username) {
+    await logout();
+  }
   return (
     <main className="flex flex-col h-screen">
       <div className="flex-1 overflow-y-auto">
-          <MessageList channelId={channelId} serverId={serverId} /> 
+        <MessageList channelId={channelId} serverId={serverId} />
       </div>
       <footer className="shrink-0 p-2 pt-0 shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1),0_-4px_6px_-2px_rgba(0,0,0,0.05)] border-t z-50">
         <MessageBox id={channelId} />
