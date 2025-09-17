59e2ad40d6778e777dca27fbf14c974fc3b9f839
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-09-17 16:37:31 +0530

    feat: add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,42 @@ import { Toaster } from "@/components/ui/sonner";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
+  alternates: { canonical: "https://bot.uncoverit.org" },
   title: "Discord Bot Client",
-  description: "Still in development",
+  description: "Interact with Discord Bots online!",
+  keywords: ["uncover it", "discord", "discord bots", "bot client"],
+  openGraph: {
+    title: "Discord Bot Client",
+    description: "Interact with Discord Bots online!",
+    url: "https://bot.uncoverit.org",
+    siteName: "Discord Bot Client",
+    images: [
+      {
+        url: "https://bot.uncoverit.org/opengraph-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large" as const,
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Discord Bot Client",
+    description: "Interact with Discord Bots online!",
+    card: "summary_large_image",
+    images: ["https://bot.uncoverit.org/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({
